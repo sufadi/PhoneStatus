5b460b88da16a35e16c086b43a4867f7de8aea68
huazhi.su <huazhi.su@tcl.com>
2019-08-09 14:54:59 +0800

    avoid killed

--- app/src/main/java/com/tct/phonedata/service/PhoneDataService.java
+++ app/src/main/java/com/tct/phonedata/service/PhoneDataService.java
@@ -1,13 +1,24 @@
 package com.tct.phonedata.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 
+import com.tct.phonedata.R;
+import com.tct.phonedata.ui.MainActivity;
 import com.tct.phonedata.utils.MyConstant;
 
 public class PhoneDataService extends Service {
+
+
+    public static final int FORGROUND_ID = 0x88;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,6 +29,7 @@ public class PhoneDataService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(MyConstant.TAG, "PhoneDataService onCreate()");
+        startMyForeground();
     }
 
     @Override
@@ -33,7 +45,38 @@ public class PhoneDataService extends Service {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         Log.d(MyConstant.TAG, "PhoneDataService onDestroy()");
+        stopForeground(true);
+        super.onDestroy();
+    }
+
+    private void startMyForeground() {
+        Log.d(MyConstant.TAG, "PhoneDataService startMyForeground sdk :" + android.os.Build.VERSION.SDK_INT);
+        Notification.Builder nb =  new Notification.Builder(this);
+
+        if (android.os.Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ONE_ID = "channel_id_foreground";
+            String CHANNEL_ONE_NAME = "Channel One";
+            NotificationChannel notificationChannel = null;
+
+            notificationChannel = new NotificationChannel(CHANNEL_ONE_ID,
+                    CHANNEL_ONE_NAME, NotificationManager.IMPORTANCE_LOW);
+
+            nb.setChannelId(CHANNEL_ONE_ID);
+
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+        nb.setSmallIcon(R.mipmap.ic_launcher);
+        nb.setContentTitle(getString(R.string.notification_title));
+        nb.setContentText(getString(R.string.notification_Content));
+        nb.setContentIntent(PendingIntent.getActivity(this,0, new Intent(this, MainActivity.class),0));
+
+        try {
+            startForeground(FORGROUND_ID, nb.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
